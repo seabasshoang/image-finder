5892d739acf98fc28f5fcdde368b959a81cbca6b
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-11-11 15:55:11 +0000

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import MuithemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+import NavBar from './components/navbar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello
-      </div>
-    );
+      <MuithemeProvider>
+        <NavBar/>
+      </MuithemeProvider>
+    )
   }
 }
 
