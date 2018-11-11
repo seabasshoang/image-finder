59e15c57bedda538b2ed49056c9e992b25cc9921
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-11-11 16:55:11 +0000

    created onChange events for search bar and amount selector

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import MuithemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import NavBar from './components/navbar/NavBar';
 import Search from './components/search/Search';
 
+
 class App extends Component {
   render() {
     return (
-      <MuithemeProvider>
-        <NavBar/>
-        <Search/>
-      </MuithemeProvider>
+        <MuiThemeProvider>
+          <div>
+            <NavBar/>
+            <Search/>
+          </div>
+        </MuiThemeProvider>
+        
     )
   }
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
 
 class Search extends Component {
 state = {
@@ -11,7 +13,18 @@ state = {
     images: []
 }
 
+onTextChange = e => {
+    this.setState({[e.target.name]: e.target.value}, () => {
+        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+        .then(res => this.setState({ images: res.data.hits }))
+        .catch(err => console.log(err));
+    });
+};
+
+onAmountChange = (e, index, value) => this.setState({ amount: value })
+
     render () {
+        console.log(this.state.images)
         return (
             <div>
                 <TextField
@@ -21,6 +34,19 @@ state = {
                     floatingLabelText="Search For Images..."
                     fullWidth={true}
                 />
+                <br/>
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}>
+                <MenuItem value={5} primaryText="5" />
+                <MenuItem value={10} primaryText="10" />
+                <MenuItem value={15} primaryText="15" />
+                <MenuItem value={30} primaryText="30" />
+                <MenuItem value={60} primaryText="60" />
+                </SelectField>
+                <br />
             </div>
         )
     };
